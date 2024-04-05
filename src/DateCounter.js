1a204959531d0e8cb5ba805c0f6d969aa2d2df58
Elena <elenaharan@gmail.com>
2024-04-05 07:16:57 +0100

    replace useState with useReducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+function reducer(currState, action) {
+  return currState + action;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
@@ -10,16 +15,18 @@ function DateCounter() {
 
   const dec = function () {
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
+    dispatch(-1);
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
+    dispatch(1);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
@@ -27,7 +34,7 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
