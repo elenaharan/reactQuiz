38adc073c35119d5aab3deb9628ab4c2f89e1607
Elena <elenaharan@gmail.com>
2024-04-06 08:50:28 +0100

    Initial project set-up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
-  return <DateCounter />;
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
 }
